359033a9db4ee7503c2bc4db4438637581f6fc13
Jonas Otto <jottosmail@gmail.com>
2018-12-14 01:48:36 +0100

    extended test script to run each profile with 10 different sizes

--- runTest.sh
+++ runTest.sh
@@ -11,32 +11,34 @@ printf "Results will be saved in $FILE\n\n";
 
 sum=0
 
-# sizes=(1000000 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1000000) 
-sizes=(1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 
- 1000000 1000000 1000000 1000000 1000000 1000000)
+# Run each of the 18 profiles with 10 different sizes (180 tests)
 
-sizeProfiles=(uniform uniform normal normal fixed8 fixed8 fixed16 fixed16 fixed24 fixed24 fixed104 fixed104
-fixed200 fixed200 increase increase decrease decrease)
-allocProfiles=(oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster
-oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster)
+sizes=(5 100 500 1000 5000 10000 50000 100000 500000 1000000 )
+sizeProfiles=(uniform uniform normal normal fixed8 fixed8 fixed16 fixed16 fixed24 fixed24 fixed104 fixed104 fixed200
+    fixed200 increase increase decrease decrease)
+allocProfiles=(oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster oneinthree
+    cluster oneinthree cluster oneinthree cluster oneinthree cluster)
 
 #Aktuelles Datum holen und notieren
 d=`date +%Y-%m-%d-%H-%M`
-echo $d > $FILE
+echo ${d} > ${FILE}
 SUMME=0
-counter=0
-while [ $counter -lt  ${#sizes[@]} ]
+c2=0
+while [[ ${c2} -lt  ${#sizes[@]} ]]
 do
-	printf "\n\033[92mTest $counter: testit %s %s %s %s\n\033[0m" "$$" "${sizes[$counter]}" "${sizeProfiles[$counter]}" "${allocProfiles[$counter]}" >> $FILE
-	VALUE=$( ./testit $$ ${sizes[$counter]} | grep '[^\.]' | tee -a $FILE | grep 'Points' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
-	# VALUE=$(./testit $$ ${sizes[$counter]} | tee -a $FILE | grep 'Points' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
-	SUMME=$(bc -l <<<"$SUMME+$VALUE")
-	printf "#"
-	((counter++))
+    counter=0
+    while [[ ${counter} -lt  ${#sizeProfiles[@]} ]]
+    do
+        printf "\n\033[92mTest $counter: testit %s %s %s %s\n\033[0m" "$$" "${sizes[$counter]}" "${sizeProfiles[$counter]}" "${allocProfiles[$counter]}" >> ${FILE}
+        VALUE=$( ./testit $$ ${sizes[c2]} | grep '[^\.]' | tee -a ${FILE} | grep 'Points' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+        # VALUE=$(./testit $$ ${sizes[$counter]} | tee -a $FILE | grep 'Points' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+        SUMME=$(bc -l <<<"$SUMME+$VALUE")
+        printf "Size $c2 Profile $counter done.\n"
+        ((counter++))
+    done
+    ((c2++))
 done
 
 printf "\nTotal sum: %s\n" "$SUMME"
-
-# read -p "Press enter to continue"
 
 exit 0;
