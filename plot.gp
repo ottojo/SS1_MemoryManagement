30d2feecd4b821e3872e2d2e9ffc2215719a71f5
ottojo <jottosmail@gmail.com>
2018-12-14 20:48:27 +0100

    fixed test script
    * fixed script ignoring profiles
    * simplified regex
    * switched to CSV output
    * added fancy graphs

---
+++ plot.gp
@@ -0,0 +1,7 @@
+set datafile separator ","
+set terminal pngcairo size 1000, 3000
+set logscale x 10
+set multiplot layout 18, 1
+do for [index=0:17] {
+    plot ARG1 i index with linespoints title columnheader(1)
+}
--- runTest.sh
+++ runTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE="TestResults.txt"
+FILE="TestResults-$(git rev-parse --short HEAD).txt"
 
 printf "Running the makefile!\n\n"
 
@@ -9,34 +9,38 @@ make -f Makefile
 printf "Finished make, continuing with tests:\n"
 printf "Results will be saved in $FILE\n\n";
 
-sum=0
+# Run each of the 18 profiles with 10 different sizes (180 tests)
 
-# sizes=(1000000 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1000000) 
-sizes=(1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 
- 1000000 1000000 1000000 1000000 1000000 1000000)
+sizes=(5 100 500 1000 5000 10000 50000 100000 500000 1000000 )
+sizeProfiles=(uniform uniform normal1 normal1 fixed8 fixed8 fixed16 fixed16 fixed24 fixed24 fixed104 fixed104 fixed200
+    fixed200 increase increase decrease decrease)
+allocProfiles=(oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster oneinthree
+    cluster oneinthree cluster oneinthree cluster oneinthree cluster)
 
-sizeProfiles=(uniform uniform normal normal fixed8 fixed8 fixed16 fixed16 fixed24 fixed24 fixed104 fixed104
-fixed200 fixed200 increase increase decrease decrease)
-allocProfiles=(oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster
-oneinthree cluster oneinthree cluster oneinthree cluster oneinthree cluster)
-
-#Aktuelles Datum holen und notieren
-d=`date +%Y-%m-%d-%H-%M`
-echo $d > $FILE
 SUMME=0
-counter=0
-while [ $counter -lt  ${#sizes[@]} ]
+profileIndex=0
+
+echo "" > ${FILE}
+
+while [[ ${profileIndex} -lt  ${#sizeProfiles[@]} ]]
 do
-	printf "\n\033[92mTest $counter: testit %s %s %s %s\n\033[0m" "$$" "${sizes[$counter]}" "${sizeProfiles[$counter]}" "${allocProfiles[$counter]}" >> $FILE
-	VALUE=$( ./testit $$ ${sizes[$counter]} | grep '[^\.]' | tee -a $FILE | grep 'Points' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
-	# VALUE=$(./testit $$ ${sizes[$counter]} | tee -a $FILE | grep 'Points' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
-	SUMME=$(bc -l <<<"$SUMME+$VALUE")
-	printf "#"
-	((counter++))
+    echo "\"${sizeProfiles[profileIndex]} ${allocProfiles[profileIndex]}\"" >> ${FILE}
+    sizeIndex=0
+    while [[ ${sizeIndex} -lt  ${#sizes[@]} ]]
+    do
+        echo "Testing Profile ${sizeProfiles[$profileIndex]} ${allocProfiles[$profileIndex]}, Size ${sizes[sizeIndex]}"
+        VALUE=$(./testit 1 ${sizes[sizeIndex]} ${sizeProfiles[$profileIndex]} ${allocProfiles[$profileIndex]} | grep '[^\.]' | grep 'Points' | grep -o -E -e '[+\-\.0-9]*')
+        SUMME=`echo ${SUMME} + ${VALUE} | bc`
+        echo "${sizes[sizeIndex]},${VALUE}" >> ${FILE}
+        echo "Profile $profileIndex, Size $sizeIndex done."
+        ((sizeIndex++))
+    done
+    printf "\n\n" >> ${FILE}
+    ((profileIndex++))
 done
 
 printf "\nTotal sum: %s\n" "$SUMME"
 
-# read -p "Press enter to continue"
+gnuplot -c plot.gp ${FILE} > "TestResults-$(git rev-parse --short HEAD).png"
 
 exit 0;
